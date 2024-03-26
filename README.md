9735e90b64d45478cd609a8194c55f418d0bf0aa
Agathe Mazuret <agathe.mazuret@gmail.com>
2024-03-26 19:08:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Projet GameOn
+1. Forkez ce repo ;
+2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
+3. Il n'y a aucune dépendance ;
+4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+# Changements
+1. Mettre les images dans un dossier "images" ;
+2. Mettre des "alt" pour les images ;
+3. Créer une fonction pour fermer la modal ;
+4. Créer une fonction validateForm pour : 
+(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+(3) L'adresse électronique est valide.
+(4) Pour le nombre de concours, une valeur numérique est saisie.
+(5) Un bouton radio est sélectionné.
+(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+
+5. Ajouter la fonction validateForm sur ma modal; 
+6. Ajout d'un message de validation du formulaire;
+7. Mettre un name à checkbox 1 et 2 ;
---
+++ modal.js
@@ -0,0 +1,48 @@
+// Cette fonction est utilisée pour modifier la classe CSS d'un élément HTML avec l'identifiant "myTopnav".
+function editNav() {
+  // Récupère l'élément HTML avec l'identifiant "myTopnav"
+  var x = document.getElementById("myTopnav");
+
+  // Vérifie si la classe de l'élément est "topnav"
+  if (x.className === "topnav") {
+    // Si oui, ajoute la classe "responsive" à la classe existante
+    x.className += " responsive";
+  } else {
+    // Sinon, remplace la classe existante par "topnav"
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+
+// Sélectionne l'élément HTML représentant le fond du modal
+const modalbg = document.querySelector(".bground");
+
+// Sélectionne tous les boutons qui déclenchent l'ouverture du modal
+const modalBtn = document.querySelectorAll(".modal-btn");
+
+// Sélectionne tous les éléments de formulaire dans le modal
+const formData = document.querySelectorAll(".formData");
+
+// Sélectionne tous les éléments de fermeture de la modal
+const closebtn = document.querySelectorAll(".close");
+
+// Écoute les événements de clic sur chaque bouton modal et lance la fonction launchModal()
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Écoute les événements de clic sur chaque bouton de fermeture et lance la fonction closeModal()
+closebtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// Cette fonction est utilisée pour afficher le modal en changeant son style pour le rendre visible.
+function launchModal() {
+  // Change le style de l'élément avec la classe "bground" pour afficher le modal
+  modalbg.style.display = "block";
+}
+
+// Cette fonction est utilisée pour cacher le modal en changeant son style pour le rendre invisible.
+function closeModal() {
+  // Change le style de l'élément avec la classe "bground" pour cacher le modal
+  modalbg.style.display = "none";
+}
+
+
