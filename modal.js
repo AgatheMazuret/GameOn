8efcfe6abe7093320e7daaf98da53aad26ad86db
Agathe Mazuret <agathe.mazuret@gmail.com>
2024-04-22 11:49:01 +0200

    Feat: add close modal functionality to modal.js

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 // Cette fonction est utilisée pour modifier la classe CSS d'un élément HTML avec l'identifiant "myTopnav".
 function editNav() {
   // Récupère l'élément HTML avec l'identifiant "myTopnav"
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
 
   // Vérifie si la classe de l'élément est "topnav"
   if (x.className === "topnav") {
@@ -44,73 +44,3 @@ function closeModal() {
   // Change le style de l'élément avec la classe "bground" pour cacher le modal
   modalbg.style.display = "none";
 }
-
-function validateForm() {
-  // Liste des champs à vérifier
-  const fields = ['first', 'last', 'email', 'birthdate', 'quantity'];
-  // Variable pour stocker les erreurs
-  let errors = '';
-
-  try {
-    // Vérification de chaque champ
-    fields.forEach(function (field) {
-      const element = document.getElementById(field);
-      const errorElement = document.getElementById('error' + field.charAt(0).toUpperCase() + field.slice(1));
-      errorElement.innerHTML = '';
-      element.classList.remove('error-field');
-
-      // Vérification du champ email et champs vides
-      if ((field === 'email' && !/\S+@\S+\.\S+/.test(element.value.trim())) || element.value.trim() === "") {
-        // Affichage de message d'erreur
-        errorElement.innerHTML = field === 'email' ? "Veuillez saisir une adresse email valide." : "Veuillez remplir ce champ.";
-        errors += field === 'email' ? "Veuillez saisir une adresse email valide.\n" : "Veuillez remplir ce champ.\n";
-        element.classList.add('error-field');
-      }
-    });
-
-    // Vérification de la sélection d'au moins un emplacement
-    const radios = document.getElementsByName('location');
-    const checkBox = document.getElementById("checkbox1");
-    let isChecked = false;
-
-    // Boucle for,  Vérifie si au moins un des emplacements est sélectionné
-    for (var i = 0; i < radios.length; i++) {
-      if (radios[i].checked) {
-        isChecked = true;
-        break;
-      }
-    }
-
-    // Vérifie si la case à cocher est cochée
-    if (!isChecked && checkBox.checked) {
-      isChecked = true;
-    }
-
-    // Affiche les messages d'erreur si aucun emplacement n'est sélectionné
-    if (!isChecked) {
-      document.getElementById('errorLocation').innerText = "Veuillez sélectionner une option.";
-      document.getElementById('errorCheckbox').innerText = "Veuillez accepter les conditions d'utilisation.";
-      return false; // Empêche la soumission du formulaire
-    } else {
-      document.getElementById('errorLocation').innerText = ""; // Réinitialise le message d'erreur
-    }
-
-    // Vérification de la case à cocher des conditions d'utilisation
-    const checkbox1 = document.getElementById("checkbox1");
-
-    // S'il y a des erreurs, lève une exception
-    if (errors !== '') {
-      throw new Error("Des champs requis ne sont pas remplis correctement.");
-    }
-
-    // Affiche un message de succès si tout est valide
-    alert("Merci ! Votre réservation a été reçue.");
-    return true;
-  } catch (error) {
-    // En cas d'erreur, affiche un message d'erreur dans la console
-    
-    return false;
-  }
-}
-
-
